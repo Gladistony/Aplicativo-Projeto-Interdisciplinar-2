5d5fa0e1a13c6af9e5cb2de440d2a14c6069f1c9
JMiguelsilva2003 <jmigueldasilva953@gmail.com>
2024-08-23 06:00:44 -0300

    adição do estilo do perfil
    css e html melhorados na tela de perfil.

--- Paginas/main-logado.php
+++ Paginas/main-logado.php
@@ -12,21 +12,13 @@ $verificado = $_SESSION['verificado'];
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bem-vindo</title>
     <script type="module" src="../Scripts/main-logado.js"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../Styles/estilo_main-logado.css">
-
 </head>
-
 <body>
     <div id="menu_do_topo" class="top-menu">
     </div>
--- Paginas/menu-perfil.php
+++ Paginas/menu-perfil.php
@@ -4,19 +4,17 @@ $email = $_SESSION['email'];
 $userInfo = $_SESSION['userInfo'];
 $verificado = $_SESSION['verificado'];
 ?>
-<section class="bg-white shadow p-4 rounded mx-auto text-center m-3 w-50">
+<link rel="stylesheet" href="../Styles/estilo_menu-perfil.css">
+<section id="perfil">
     <div class="d-inline-block">
-        <img src="https://images.unsplash.com/photo-1567324216289-97cc4134f626?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="User Profile Picture" class="img-fluid rounded-circle"
-            style="width: 250px; height: 250px; object-fit: cover;" />
+        <img src="https://images.unsplash.com/photo-1567324216289-97cc4134f626?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
     </div>
-    <h2 class="mt-2"><?php echo htmlspecialchars($userInfo['nome']); ?></h2>
-    <h5><strong><?php echo htmlspecialchars($email); ?></strong></h5>
+    <h1 class="mt-2"><?php echo htmlspecialchars($userInfo['nome']); ?></h1>
+    <h2><?php echo htmlspecialchars($email); ?></h2>
     <p class="mb-1">Cursando <?php echo htmlspecialchars($userInfo['curso']); ?></p>
     <p class="mb-1">Período de ingresso: 2024.1</p>
     <p class="mb-1">Data de nascimento: 17/06/2001</p>
-    <p class="mb-1">Endereço: Rua Moradas, nº 123 - Belo Jardim, PE</p>
-    <h4 class="mb-1 mt-3">Descrição</h4>
+    <h3 class="mb-1 mt-3">Descrição</h3>
     <textarea class="p-2 border border-dark rounded w-75" placeholder="Fale mais sobre você..."></textarea>
     <div class="d-grid gap-2 mt-4 w-50 mx-auto">
         <button class="btn btn-light border border-dark text-dark px-4 rounded">
