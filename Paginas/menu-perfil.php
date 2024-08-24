021adacc1bcb5eee39ff1552e1f42d18138861f2
Erick Jonathan <65685466+Erickjonatthan@users.noreply.github.com>
2024-08-24 12:50:42 -0300

    voltando para o php na pagina de perfil

--- Paginas/menu-perfil.php
+++ Paginas/menu-perfil.php
@@ -11,30 +11,32 @@ $dataCriacao = isset($userInfo['dataCriacao']) ? $userInfo['dataCriacao'] : 'Dat
 ?>
 <link rel="stylesheet" href="../Styles/estilo_menu-perfil.css">
 <section id="perfil">
-    <div class="d-inline-block">
+    <div class="imagem">
         <img id="profileImage" src="<?php echo htmlspecialchars($fotoPerfil); ?>" />
     </div>
-    <h1 class="mt-2"><?php echo htmlspecialchars($userInfo['nome']); ?></h1>
+    <h1><?php echo htmlspecialchars($userInfo['nome']); ?></h1>
     <h2><?php echo htmlspecialchars($email); ?></h2>
     <?php if (!$verificado) : ?>
         <div class="alert">
-            <h2>Sua conta ainda não está verificada. Por favor, verifique seu e-mail.</h2>
+            <h4>Sua conta ainda não está verificada. Por favor, verifique seu e-mail.</h4>
         </div>
     <?php endif; ?>
-    <p class="mb-1">Cursando <?php echo htmlspecialchars($userInfo['curso']); ?></p>
-    <p class="mb-1">Período de ingresso: <input type="text" id="periodo" value="<?php echo htmlspecialchars($periodo); ?>" readonly></p>
-    <p class="mb-1">Data de nascimento: <input type="date" id="nascimento" value="<?php echo htmlspecialchars($nascimento); ?>" readonly></p>
-    <p class="mb-1">Data de criação do perfil: <?php echo htmlspecialchars($dataCriacao); ?></p>
-    <h3 class="mb-1 mt-3">Descrição</h3>
+    <p>Cursando <?php echo htmlspecialchars($userInfo['curso']); ?></p>
+    <p>Período de ingresso: <input type="text" id="periodo" value="<?php echo htmlspecialchars($periodo); ?>" readonly></p>
+    <p>Data de nascimento: <input type="date" id="nascimento" value="<?php echo htmlspecialchars($nascimento); ?>" readonly></p>
+    <p>Data de criação do perfil: <?php echo htmlspecialchars($dataCriacao); ?></p>
+    <h3>Descrição</h3>
     <textarea id="descricao" placeholder="Fale mais sobre você..." readonly><?php echo htmlspecialchars($descricao); ?></textarea>
-    <div class="d-grid gap-2 mt-4 w-50 mx-auto">
+    <div class="button-container">
         <input type="file" id="profilePictureInput" accept="image/*" style="display: none;" />
         <div id="loading-fotoperfil" class="loading-spinner" style="display: none;"></div>
         <button class="btn-editar" id="botaoloadfoto" onclick="document.getElementById('profilePictureInput').click();">
+            <img id="botaoloadfoto" class="icone" src="../Recursos/Imagens/editar.png" alt="icone-editar">
             Editar minha foto de perfil
         </button>
-        <button class="btn-editar" id="editarPerfilBtn">
-            Editar perfil
+        <button class="btn-editar">
+            <img id="botaoloadend" class="icone" src="../Recursos/Imagens/fixar-mapa.png" alt="icone-endereco">
+            Editar Endereço
         </button>
     </div>
 </section>
