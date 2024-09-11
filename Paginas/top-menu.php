f811a4e31aa9fe3fb9256bd74a59cc4352089fb7
Pedro  Emmanuel G. C. Machado <132278032+Pedro-Emmanuel-G-C-Machado@users.noreply.github.com>
2024-09-11 10:12:44 -0300

    Mudanças Na tela

--- Paginas/top-menu.php
+++ Paginas/top-menu.php
@@ -6,13 +6,19 @@ $fotoPerfil = isset($userInfo['fotoPerfil']) ? $userInfo['fotoPerfil'] : '../Rec
 $nomeUsuario = isset($userInfo['nome']) ? $userInfo['nome'] : 'Usuário';
 ?>
 <section id="menu-topo">
-    <img alt="Foto do Usuário" class="user-photo" src="<?php echo htmlspecialchars($fotoPerfil); ?>" />
+    <a href="./Paginas/menu-perfil.php">
+        <img alt="Foto do Usuário" class="user-photo" src="<?php echo htmlspecialchars($fotoPerfil); ?>" />>
+
+    </a>
+
     <div class="user-profile">
         <button id="botao-drop-down">
             <span class="user-name"><?php echo htmlspecialchars($nomeUsuario); ?></span>
-            <span class="arrow-down"><img src="../Recursos/Imagens/Seta-para-baixo.jpeg" alt=""></span> <!-- Unicode for modifier letter down arrowhead -->
+            <span class="arrow-down"><img src="../Recursos/Imagens/Seta-para-baixo.jpeg" alt=""></span>
+            <!-- Unicode for modifier letter down arrowhead -->
         </button>
         <div class="dropdown" id="dropdown" style="display: none;">
+            <button id="botao-drop-seu-Perfil"> Seu perfil</button>
             <button id="botao-drop-sair">Sair</button>
         </div>
     </div>
