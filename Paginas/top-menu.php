387ed2aac24668122ffe56a4ed8851822c92fad9
Pedro  Emmanuel G. C. Machado <132278032+Pedro-Emmanuel-G-C-Machado@users.noreply.github.com>
2024-09-12 09:19:58 -0300

    Colocando limitação de nome e sobrenome

--- Paginas/top-menu.php
+++ Paginas/top-menu.php
@@ -2,8 +2,13 @@
 session_start();
 
 $userInfo = $_SESSION['userInfo'];
+$nomeCompleto = isset($userInfo['nome']) ? $userInfo['nome'] : 'Usuário';
+$partesNome = explode(' ', $nomeCompleto);
+$primeiroNome = $partesNome[0];
+$ultimoNome = end($partesNome);
+$nomeUsuario = $primeiroNome . ' ' . $ultimoNome;
+
 $fotoPerfil = isset($userInfo['fotoPerfil']) ? $userInfo['fotoPerfil'] : '../Recursos/Imagens/perfil-teste.avif';
-$nomeUsuario = isset($userInfo['nome']) ? $userInfo['nome'] : 'Usuário';
 ?>
 <section id="menu-topo">
     <a href="./Paginas/menu-perfil.php">
@@ -15,12 +20,10 @@ $nomeUsuario = isset($userInfo['nome']) ? $userInfo['nome'] : 'Usuário';
         <button id="botao-drop-down">
             <span class="user-name"><?php echo htmlspecialchars($nomeUsuario); ?></span>
             <span class="arrow-down"><img src="../Recursos/Imagens/Seta-para-baixo.jpeg" alt=""></span>
-            <!-- Unicode for modifier letter down arrowhead -->
         </button>
         <div class="dropdown" id="dropdown" style="display: none;">
             <a href="./Paginas/menu-perfil.php" id="botao-drop-seu-Perfil" class="botao-link">Seu perfil</a>
             <button id="botao-drop-sair">Sair</button>
-
         </div>
     </div>
 </section>
